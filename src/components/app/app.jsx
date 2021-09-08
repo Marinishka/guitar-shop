0e6ec9314a1016b4443287cc03978d24c84d2bd8
Marinishka <shipovalova2015@yandex.ru>
2021-09-08 15:04:41 +0300

    Созданы компненты: шапка и подвал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
+import Footer from '../footer/footer';
+import Header from './../header/header';
 
-    </div>
+function App() {
+  return (<>
+    <Header/>
+    <Footer/>
+  </>
   );
 }
 
